7f86f1fea9c3b1d5b96a9072499904a83f9df4df
ZavyalovAndrei <zaver1983@google.com>
2023-09-30 01:01:53 +0200

    Eighth task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,43 @@
 //Задача № 1
-function cachingDecoratorNew(func) {
-  
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+    if (timeoutId === null) {
+      wrapper.count++;
+      func(...args);
+    }
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  return wrapper;
 }
 
 //Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+function cachingDecoratorNew(func) {
+  let cache = [];
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      return "Из кеша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({
+      hash,
+      value: result,
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
